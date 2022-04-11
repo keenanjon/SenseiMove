c968bfdb3920e3118cad3eb6dceb02b23e5bf9b1
keenanjon <jon.menna@metropolia.fi>
2022-04-11 15:09:48 +0300

    Initial Commit

---
+++ MoveSenseiData/MoveSenseiDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveSenseiDataApp.swift
+//  MoveSenseiData
+//
+//  Created by Jon Menna on 11.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveSenseiDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
